35ca6321b75a80e2f96717f100c0b6dd82fbe4af
EnockOMONDI <djseanizellkenya@gmail.com>
2018-05-28 10:01:34 +0300

    added business class and hood member class

--- neighbourhoodapp/models.py
+++ neighbourhoodapp/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.utils.text import slugify
+from django import template
+from django.contrib.auth import get_user_model
+from django.core.urlresolvers import reverse
+
 
 # Create your models here.
+User = get_user_model()
+reigster = template.Library
+
+
+class Neighbourhood(models.Model):
+    name = models.CharField(max_length=140, unique=True)
+    slug = models.SlugField(allow_unicode=True,  unique=True)
+    location = models.CharField(max_length=140, blank=True, default='')
+    occupants = models.ManyToManyField(User, through='NeighbourhoodMember')
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        return reverse('neighbourhoods:single', kwargs={'slug': self.slug})
+
+    class Meta:
+        ordering = ['name']
+
+
+class NeighbourhoodMember(models.Model):
+    neighbourhood = models.ForeignKey(Neighbourhood, related_name='memberships')
+    user = models.ForeignKey(User, related_name='user_neighbourhoods')
+
+    def __str__(self):
+        return self.user.username
+
+    class Meta:
+        unique_together = ('neighbourhood', 'user')
