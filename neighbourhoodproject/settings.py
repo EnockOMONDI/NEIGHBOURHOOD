97a4ddd51cb3aaca8619bb9b5db5e8a59de90a11
EnockOMONDI <djseanizellkenya@gmail.com>
2018-05-28 07:20:30 +0300

    added my apps in my project settings

--- neighbourhoodproject/settings.py
+++ neighbourhoodproject/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'posts'
+    'neighbourhoodapp'
+    'bootstrap4'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
