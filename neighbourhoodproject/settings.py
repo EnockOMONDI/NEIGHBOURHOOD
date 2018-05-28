59f93dcd2762580cd32f53ab757115920ec32451
EnockOMONDI <djseanizellkenya@gmail.com>
2018-05-28 08:02:47 +0300

    fixed django project installed app settings and reinstalled my apps

--- neighbourhoodproject/settings.py
+++ neighbourhoodproject/settings.py
@@ -31,10 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-
-    'neighbourhoodapp'
-    'posts'
-    'bootstrap4'
+    'neighbourhoodapp',
+    'posts',
+    'bootstrap4',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
