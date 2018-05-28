44d7505d07469a32bdf46c1911e74e1ed044ce4e
EnockOMONDI <djseanizellkenya@gmail.com>
2018-05-28 20:01:15 +0300

    registered neighbourhoodmember model in admin

--- neighbourhoodapp/admin.py
+++ neighbourhoodapp/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+
+
+class NeighbourhoodMember(admin.TabularInline):
+    model = models.NeighbourhoodMember
+
+
+admin.site.register(models.Neighbourhood)
