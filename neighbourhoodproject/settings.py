4b131a04e9a1bdf9a123edbc82c3da972740fabe
EnockOMONDI <djseanizellkenya@gmail.com>
2018-05-28 07:42:24 +0300

    fixed installed apps error

--- neighbourhoodproject/settings.py
+++ neighbourhoodproject/settings.py
@@ -31,8 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'posts'
+
     'neighbourhoodapp'
+    'posts'
     'bootstrap4'
     'django.contrib.admin',
     'django.contrib.auth',
