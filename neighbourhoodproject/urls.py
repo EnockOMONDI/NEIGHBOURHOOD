d38368fbdc655001b224d821751c6ecd9151aef9
EnockOMONDI <djseanizellkenya@gmail.com>
2018-05-28 19:36:01 +0300

    added all urls map

--- neighbourhoodproject/urls.py
+++ neighbourhoodproject/urls.py
@@ -22,8 +22,6 @@ urlpatterns = [
     url(r'^$', views.HomePage.as_view(), name='home'),
     url(r'^accounts/', include('accounts.urls', namespace='accounts')),
     url(r'^accounts/', include('django.contrib.auth.urls')),
-    url(r'^accounts/', include('accounts.urls', namespace='accounts')),
-    url(r'^accounts/', include('django.contrib.auth.urls')),
     url(r'^posts/', include('posts.urls', namespace='posts')),
     url(r'^neighbourhoods/', include('neighbourhoods.urls', namespace='neighbourhoods'))
     ]
