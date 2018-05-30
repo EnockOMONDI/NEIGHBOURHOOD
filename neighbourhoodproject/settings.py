1125789754ce876adaa24191bcb6843e4d2ae2af
EnockOMONDI <djseanizellkenya@gmail.com>
2018-05-30 11:27:36 +0300

    added  runtime.txt and procfile

--- neighbourhoodproject/settings.py
+++ neighbourhoodproject/settings.py
@@ -125,7 +125,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_URL = '/static/'
 STATICFILES_DIR = [os.path.join(BASE_DIR, 'static')]
 
