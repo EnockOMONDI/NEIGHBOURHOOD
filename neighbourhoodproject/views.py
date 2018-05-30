cdd305b4ba062dde443a57a93129dbcc8911f02d
EnockOMONDI <djseanizellkenya@gmail.com>
2018-05-30 15:16:03 +0300

    added secret key

--- neighbourhoodproject/views.py
+++ neighbourhoodproject/views.py
@@ -1,6 +1,8 @@
 from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
-
+'''
+this is the view responsible for the welcome page
+'''
 
 class HomePage(LoginRequiredMixin, TemplateView):
     template_name = 'index.html'
